77da54a7ae5879289285142c5f5ebeaf93c65974
GabrielSCF-UFMG <42360613+GabrielSCF-UFMG@users.noreply.github.com>
2026-06-03 14:22:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'planejamento-mergulho-cbmmg-v2.6';
+
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './anexos-a-b.js',
+    './manifest.json',
+    './brasao-cbmmg.png',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => cacheName !== CACHE_NAME)
+                    .map((cacheName) => caches.delete(cacheName))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
+});
